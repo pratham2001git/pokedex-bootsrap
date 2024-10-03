db05f0eb1bb24b8b842da3d9807b2243fe447746
Pratham Gonate <Prathamgonte@proton.me>
2024-10-03 08:08:20 +0000

    pokemon update

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,10 @@
-import data from "./data.json"
-import PokemonCard from "./components/PokemonCard"
+import data from "./data.json"  
+import {Pokemon} from "./components/PokemonCard"
+import arrayShuffle from "array-shuffle";
+
+const inputE1 = document.querySelector("input")
+
+
 
 // DOM selection
 // console.log(data-pokemon-row)
@@ -9,11 +14,32 @@ import PokemonCard from "./components/PokemonCard"
 
 
   function renderPokemon(list){
+  dataRow.textContent = "";
     list.forEach((pokemonObj) =>{
       Pokemon(pokemonObj);
     })
   }
-  renderPokemon(data);
 
+  function handleSearch(input){
+    const filteredPokemon = data.filter((pokemonObj)=>{
+     return pokemonObj.name.toLowerCase().includes(input)
+    });
+    renderPokemon(filteredPokemon);
+  }
 
+  inputE1.addEventListener("input",(e)=>{
+     handleSearch(e.target.value.trim().toLowerCase());
+     handleSearch(currentInput)
+    
+  })
+
+renderPokemon(arrayShuffle((data)));
  
+
+// add slash to active search 
+ document.addEventListener("keydown",(e)=>{
+   if(e.key === "/"){
+    e.preventDefault();
+    inputE1.focus();
+   }
+ });
